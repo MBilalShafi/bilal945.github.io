c32e461351889ee60a768b5f031c91f6d8f318f3
Bilal Shafi <bilal.shafi@aeratechnology.com>
2021-12-17 12:44:37 +0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3935812bff21f8f78019.js"
+    "url": "webpack-runtime-f1667445303024878739.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0fae02d8ab608b7d7a83ded8549310e5"
+    "revision": "3c0ba4c28fc2f93169dbf933070a9ef8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-260e89c911c4cfae04b1.js"
